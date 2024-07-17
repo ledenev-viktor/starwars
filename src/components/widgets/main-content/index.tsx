eba575e8d2dafc5b7c9e0c4e99cfb3505856e503
ledenev-viktor <ledenev4252@gmail.com>
2024-07-18 00:40:19 +0300

    example load more list by button

--- src/components/widgets/main-content/index.tsx
+++ src/components/widgets/main-content/index.tsx
@@ -1,15 +1,27 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useFetchPeople } from '../../../api/hooks/useFetchPeople';
-import { Flex, List, PaginationProps, Typography } from 'antd';
-import { extractNumberFromUrl } from '../../utils';
-import { Input, Card, Pagination, Spin } from '../../ui';
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { Flex, List, ListProps, PaginationProps, Typography } from 'antd';
+import { useFetchPeople, useLocalStorageState } from '~hooks';
+import { Input, Card, Pagination, Spin } from '~ui';
 import { debounce } from 'lodash';
-import { useLocalStorageState } from '../../../hooks/useLocalStorageState';
+
+import axios from 'axios';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 export const MainContent = () => {
   const [page, setPage] = useLocalStorageState('pager', 1);
   const [searchValue, setSearchValue] = useState('');
   const [debouncedVearchValue, setDebouncedSearchValue] = useState('');
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [listItems, setListItems] = useState([]);
+  const listRef = useRef<any>(null);
+
   const { data: people, isLoading: isLoadingPeople } = useFetchPeople({
     page,
     search: debouncedVearchValue,
@@ -30,14 +42,77 @@ export const MainContent = () => {
     debouncedSetSearch(searchValue);
   }, [searchValue, debouncedSetSearch]);
 
+  const fetchListItems = async ({ pageParam }: { pageParam: number }) => {
+    console.log('pageParam', pageParam);
+    const { data: response } = await axios(__BASE_URL__, {
+      params: { page: pageParam },
+    });
+    // setListItems([...listItems, ...response.results]);
+    console.log('response', response);
+    const { results, count } = response;
+    return { results, count };
+  };
+
+  // useEffect(() => {
+  //   fetchListItems(currentPage);
+  // }, [currentPage]);
+
+  console.log('data', listItems);
+
+  const getMoreItem = () => {
+    if (listRef.current) {
+      const list = listRef.current.getBoundingClientRect();
+      console.log(list.bottom);
+      if (list.bottom * 0.9 < window.innerHeight) {
+        setCurrentPage(prevState => prevState + 1);
+      }
+    }
+  };
+
+  const { data, status, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ['li'],
+      queryFn: fetchListItems,
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPage) => {
+        const allPages = allPage.reduce((acc, {results}) => {
+          acc.push(...results);
+          return acc;
+        }, []);
+
+        const nextPage = allPages.length !== lastPage.count
+          ? allPage.length + 1
+          : undefined;
+
+        return nextPage;
+      },
+    });
+  console.log('data => ', data);
+
+  const content = data?.pages.map(items =>
+    items.results.map(item => {
+      return (
+        <li key={item.url}>
+          <Card href={'/people/' + item.url.split('/').find(Number)}>
+            {item.name}
+          </Card>
+        </li>
+      );
+    }),
+  );
+
   if (isLoadingPeople) {
-    return <Flex justify='center'><Spin/></Flex>;
+    return (
+      <Flex justify="center">
+        <Spin />
+      </Flex>
+    );
   }
 
   return (
-    <Flex vertical>
+    <Flex ref={listRef} vertical gap={20}>
       <Typography.Title
-        style={{ color: '#fff', textAlign: 'center' }}
+        style={{ color: '#fff', textAlign: 'center', margin: 0 }}
         level={1}
       >
         Stars Wars
@@ -46,7 +121,6 @@ export const MainContent = () => {
         placeholder="search"
         value={searchValue}
         onChange={event => setSearchValue(event.target.value)}
-        style={{ marginBottom: '20px' }}
       />
       <List
         grid={{
@@ -68,13 +142,43 @@ export const MainContent = () => {
         dataSource={people?.results}
         renderItem={(item: { [key: string]: string }) => (
           <List.Item>
-            <Card href={'/people/' + extractNumberFromUrl(item.url)}>
+            <Card href={'/people/' + item.url.split('/').find(Number)}>
               {item.name}
             </Card>
           </List.Item>
         )}
       />
-      <Pagination
+      <ul>{content}</ul>
+
+      {hasNextPage &&
+        <button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? 'load more...' : 'load more'}
+        </button>
+      }
+      {/* {listItems?.length > 0 && (
+        <div>
+          <ul>
+            {listItems?.map(item => {
+              return (
+                <li key={item.url.split('/').find(Number)}>
+                  <Card href={'/people/' + item.url.split('/').find(Number)}>
+                    {item.name}
+                  </Card>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+      <button
+        onClick={() => {
+          setCurrentPage(prev => prev + 1);
+        }}
+      >
+        load
+      </button> */}
+
+      {/* <Pagination
         onChange={handlePager}
         defaultCurrent={1}
         current={page}
@@ -82,7 +186,7 @@ export const MainContent = () => {
         hideOnSinglePage={true}
         responsive={true}
         showSizeChanger={false}
-      />
+      /> */}
     </Flex>
   );
 };
