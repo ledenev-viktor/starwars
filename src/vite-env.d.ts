7116a8bd0ba6d9bc749a4bececf8742df2512aa9
ledenev-viktor <ledenev4252@gmail.com>
2024-07-19 02:59:15 +0300

    fix errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,4 @@
 /// <reference types="vite/client" />
-declare const __BASE_URL__: string
+declare const __BASE_URL__: string;
+declare const __HOME_URL__: string;
+declare const __BACKGROUNDS_IMG__: string[];
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const commonConfig = {
     define: {
-      '__BASE_URL__': JSON.stringify('https://swapi.dev/api/people/'),
+      __BASE_URL__: JSON.stringify('https://swapi.dev/api/people/'),
+      __HOME_URL__: mode === 'production' ? JSON.stringify('/starwars/') : JSON.stringify('/'),
+      __BACKGROUNDS_IMG__:
+        mode === 'production'
+          ? [
+              'https://raw.githubusercontent.com/ledenev-viktor/starwars/main/public/bg1.webp',
+              'https://raw.githubusercontent.com/ledenev-viktor/starwars/main/public/bg2.webp',
+              'https://raw.githubusercontent.com/ledenev-viktor/starwars/main/public/bg3.webp',
+            ]
+          : ['/bg1.webp', '/bg2.webp', '/bg3.webp'],
     },
     resolve: {
       alias: {
@@ -16,15 +25,15 @@ export default defineConfig(({mode}) => {
         '~components': path.resolve(__dirname, 'src/components/'),
         '~styles': path.resolve(__dirname, 'src/styles/'),
         '~pages': path.resolve(__dirname, 'src/pages/'),
-      }
-    }
-  }
+      },
+    },
+  };
 
   if (mode === 'development') {
     return {
       ...commonConfig,
       plugins: [react()],
-      base: '',
+      base: '/',
     };
   } else if (mode === 'production') {
     return {
@@ -33,7 +42,6 @@ export default defineConfig(({mode}) => {
       base: '/starwars/',
     };
   } else {
-    throw new Error(`unexpected mode ${mode}`)
+    throw new Error(`unexpected mode ${mode}`);
   }
-
 });
